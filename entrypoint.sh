cae9e7b4d09e5cb37f02c890c651ccfb6f45740e
Nguyễn Quốc  Khánh <88977427+QuocKhanh209@users.noreply.github.com>
2025-04-26 14:50:11 +0700

    Tải dữ liệu mới nhất của Việt Nam

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Đường dẫn tới thư mục chứa file OSM
 OSM_DIR="/app/osm-data"
-OSM_FILE="$OSM_DIR/berlin-latest.osm.pbf"
+OSM_FILE="$OSM_DIR/vietnam-latest.osm.pbf"
 
 # Tạo thư mục nếu chưa tồn tại
 mkdir -p "$OSM_DIR"
@@ -10,7 +10,7 @@ mkdir -p "$OSM_DIR"
 # Kiểm tra nếu file OSM chưa tồn tại thì tải về
 if [ ! -f "$OSM_FILE" ]; then
     echo "Đang tải dữ liệu OSM từ Geofabrik..."
-    wget http://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf -O "$OSM_FILE"
+    wget https://download.geofabrik.de/asia/vietnam-latest.osm.pbf -O "$OSM_FILE"
     if [ $? -ne 0 ]; then
         echo "Lỗi: Không thể tải tệp OSM!"
         exit 1
